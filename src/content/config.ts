253798f050e7e0cf1f84e7235daa27a698916da4
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-06-26 01:37:29 +0200

    feat: load data

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,70 @@
-// src/content/config.ts
+import { highlights } from '../constants';
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
+import fs from 'fs';
+import type { RepoReference } from '../types';
+import categories from '../constants/categories';
+
+const loadFromRepoReference = (repo: RepoReference) => {
+  const filename = `./data/repos/${repo.usr}.${repo.repoName}.json`;
+  if (!fs.existsSync(filename)) {
+    return null;
+  }
+  const data = fs.readFileSync(filename, { encoding: 'utf-8' });
+  return {
+    id: `${repo.usr}/${repo.repoName}`,
+    ...JSON.parse(data)
+  } as { id: string, data: RepoReference };
+};
+
+const repoSchema = z.object({
+  type: z.enum(['github']),
+  name: z.string(),
+  full_name: z.string(),
+  url: z.string(),
+  description: z.string(),
+  highlights: z.array(z.string()).optional(),
+  tags: z.array(z.string()),
+  links: z.array(z.object({
+    type: z.enum(['docs']).optional(),
+    url: z.string(),
+    text: z.string(),
+  }))
+});
+
+const highlightsCollection = defineCollection({
+  loader: async () => {
+    const data = await Promise.all(highlights.map(loadFromRepoReference));
+    return data.filter(Boolean) as { id: string, data: RepoReference }[];
+  },
+  schema: repoSchema
+});
+
+const categoriesCollection = defineCollection({
+  loader: async () => {
+    const data = await Promise.all(categories.map(async category => {
+      const repos = await Promise.all(category.repos.map(loadFromRepoReference));
+      return {
+        id: category.title,
+        title: category.title,
+        repos: repos.filter(Boolean)
+      };
+    }));
+    return data;
+  },
+  schema: z.object({
+    title: z.string(),
+    repos: z.array(repoSchema)
+  })
+})
 
 const repos = defineCollection({
   loader: glob({ pattern: '**/*.json', base: './data/repos' }),
-  schema: z.object({
-    type: z.enum(['github']),
-    name: z.string(),
-    full_name: z.string(),
-    url: z.string(),
-    description: z.string(),
-    highlights: z.array(z.string()).optional(),
-    tags: z.array(z.string()),
-    links: z.array(z.object({
-      type: z.enum(['docs']).optional(),
-      url: z.string(),
-      text: z.string(),
-    }))
-  }),
+  schema: repoSchema,
 });
 
 export const collections = {
   repos,
+  highlights: highlightsCollection,
+  categories: categoriesCollection
 };
